dba3543131930367a8801756e0ca19a9b7b81446
tran-hong-khanh <62730991+tran-hong-khanh@users.noreply.github.com>
2022-04-07 07:56:34 +0700

    add my flow

--- prefectStorate.py
+++ prefectStorate.py
@@ -1,1 +1,32 @@
 
+from prefect import task, Flow
+import prefect
+from prefect.schedules import IntervalSchedule
+from datetime import timedelta
+import datetime
+from prefect.agent.docker import DockerAgent
+from prefect.storage import Local
+from prefect.run_configs import UniversalRun
+from prefect.storage import GitLab
+
+@task
+def hello_task():
+    logger = prefect.context.get("logger")
+    logger.info("Hello world!")
+    
+# schedule to run every 2 minutes
+schedule = IntervalSchedule(
+    start_date=datetime.datetime.utcnow() + timedelta(seconds=1),
+    interval=timedelta(minutes = 2))
+
+# define Prefect flow
+# with Flow("hello-flow", schedule=schedule, run_config=UniversalRun()) as flow:
+with Flow("hello-flow", schedule=schedule) as flow:
+    hello_task()
+flow.storage = GitLab(
+            repo="269",
+            path="prefectStorate.py",
+            host="https://gitlab.vmgmedia.vn/",
+            access_token_secret="glpat-su5uaLifXGHDhJXQRnfM"
+        )
+flow.run_config = UniversalRun(["vmg-B560M-AORUS-ELITE"])
